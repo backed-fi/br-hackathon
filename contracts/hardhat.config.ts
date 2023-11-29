3492946061725048dd01535efa4ed5a42fc89df0
jdrskr <jedrzej.skrodzki@gmail.com>
2023-11-29 22:04:48 +0100

    feat: hide networks when build from CI pipeline

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -26,36 +26,41 @@ const config: HardhatUserConfig = {
     }
   },
   networks: {
-    hardhat: {
-      forking: { url: 'https://endpoints.omniatech.io/v1/eth/sepolia/public	'},
-      allowUnlimitedContractSize: true
-    },
-    localhost: {
-      url: 'http://localhost:8545',
-      accounts: [process.env.SEPOLIA_PK!]
-    },
-    sepolia: {
-      url: 'https://ethereum-sepolia.publicnode.com',
-      accounts: [process.env.SEPOLIA_PK!]
-    },
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PK!]
-    }
+    ...(process.env.CI ? {} : {
+      hardhat: {
+        forking: { url: 'https://endpoints.omniatech.io/v1/eth/sepolia/public	' },
+        allowUnlimitedContractSize: true
+      },
+      localhost: {
+        url: 'http://localhost:8545',
+        accounts: [process.env.SEPOLIA_PK!]
+      },
+      sepolia: {
+        url: 'https://ethereum-sepolia.publicnode.com',
+        accounts: [process.env.SEPOLIA_PK!]
+      },
+      mainnet: {
+        url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+        accounts: [process.env.PK!]
+      }
+    })
+
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
+    // apiKey: process.env.ETHERSCAN_API_KEY
   },
   paths: {
     sources: './contracts',
     artifacts: `./artifacts`,
     cache: `./cache`
   },
-  ...(process.env.FRONTEND_BUILD && {typechain: {
-    outDir: `./../frontend/src/typechain`,
-    target: 'ethers-v5',
-    alwaysGenerateOverloads: true
-  }}),
+  ...(process.env.FRONTEND_BUILD && {
+    typechain: {
+      outDir: `./../frontend/src/typechain`,
+      target: 'ethers-v5',
+      alwaysGenerateOverloads: true
+    }
+  }),
 };
 
 export default config;
